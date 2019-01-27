5e2344d997c2e5414a4c925d3da378ce1bf8cea0
Claudio Ferreira <claudio.zer0@gmail.com>
2019-01-27 13:32:20 -0200

    ngx-toastr
    Inclusão do ngx-toastr

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,9 @@ import { CadastroChamadoComponent } from './cadastro-chamado/cadastro-chamado.co
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { PainelChamadosComponent } from './painel-chamados/painel-chamados.component';
 import { BsDatepickerModule } from 'ngx-bootstrap/datepicker';
+import { CommonModule } from '@angular/common';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ToastrModule } from 'ngx-toastr';
 
 @NgModule({
   declarations: [
@@ -20,6 +23,9 @@ import { BsDatepickerModule } from 'ngx-bootstrap/datepicker';
     FormsModule,
     ReactiveFormsModule,
     BsDatepickerModule.forRoot(),
+    CommonModule,
+    BrowserAnimationsModule, // required animations module
+    ToastrModule.forRoot() // ToastrModule added
   ],
   providers: [],
   bootstrap: [AppComponent]
