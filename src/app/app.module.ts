c5b67887bcf7f832e245bae26efc5eefbf3e3aeb
Claudio Ferreira <claudio.zero0@gmail.com>
2019-01-28 15:43:42 -0200

    Dialog Alert
    Criado o componente dialog-alert

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,12 +10,14 @@ import { BsDatepickerModule } from 'ngx-bootstrap/datepicker';
 import { CommonModule } from '@angular/common';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { ToastrModule } from 'ngx-toastr';
+import { DialogAlertComponent } from './dialog-alert/dialog-alert.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     CadastroChamadoComponent,
-    PainelChamadosComponent
+    PainelChamadosComponent,
+    DialogAlertComponent
   ],
   imports: [
     BrowserModule,
